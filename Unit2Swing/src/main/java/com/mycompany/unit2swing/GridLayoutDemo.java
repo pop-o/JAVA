6ba3b99d839e4fa26ee8bd56a67ba52dc4c0a751
Poem Maharjan <poemmahrajan4290@gmail.com>
2024-08-02 09:40:21 +0545

    Layouts

---
+++ Unit2Swing/src/main/java/com/mycompany/unit2swing/GridLayoutDemo.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.unit2swing;
+/*
+Places the content in form of row and column
+i.e. like table format
+*/
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+class GridL extends JFrame{
+    JButton b1,b2,b3,b4,b5;
+    JTextArea t1;
+    public void setGrid(){
+        b1=new JButton("Button 1");
+        b2=new JButton("Button 2");
+        b3=new JButton("Button 3");
+        b4=new JButton("Button 4");
+        b5=new JButton("Button 5");
+        t1=new JTextArea(10,10);
+        
+        add(b1);
+        add(b2);
+        add(b3);
+        add(b4);
+        add(b5);
+        add(t1);
+        
+        setVisible(true);
+        setDefaultCloseOperation(3);
+        setSize(500,500);
+        setLayout(new GridLayout(3,2));
+    }
+    
+    
+}
+
+public class GridLayoutDemo {
+    public static void main(String[] args) {
+        GridL g=new GridL();
+        g.setGrid();
+    }
+}
